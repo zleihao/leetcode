2e09b25113cd5d14c1db04b21334d85403249946
hao <im_leihao@163.com>
2022-07-30 21:48:30 +0800

    添加了有效的括号

---
+++ rust_leetcode/src/bin/is_valid.rs
@@ -0,0 +1,50 @@
+struct Solution;
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack: Vec<char> = Vec::with_capacity(s.len());
+
+        for c in s.chars() {
+            match stack.last() {
+                Some(&v) => {
+                    if v == '(' && c == ')' {
+                        stack.pop();
+                    } else if v == '{' && c == '}' {
+                        stack.pop();
+                    } else if v == '[' && c == ']' {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                }
+                None => stack.push(c),
+            }
+        }
+        if stack.is_empty() {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn main() {
+    //示例 1：
+    let s = "()";
+    assert_eq!(true, Solution::is_valid(s.to_string()));
+
+    // 示例 2：
+    let s = "()[]{}";
+    assert_eq!(true, Solution::is_valid(s.to_string()));
+
+    // 示例 3：
+    let s = "(]";
+    assert_eq!(false, Solution::is_valid(s.to_string()));
+
+    // 示例 4：
+    let s = "([)]";
+    assert_eq!(false, Solution::is_valid(s.to_string()));
+    
+    // 示例 5：
+    let s = "{[]}";
+    assert_eq!(true, Solution::is_valid(s.to_string()));
+}
