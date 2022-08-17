31c2e81ff96d2e39b6b72050a2fdd53230f84ac8
hao <im_leihao@163.com>
2022-08-17 20:20:33 +0800

    add 268. 丢失的数字

---
+++ rust_leetcode/src/bin/missing_number.rs
@@ -0,0 +1,26 @@
+struct Solution;
+
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        (0..=nums.len() as i32).sum::<i32>() - nums.iter().sum::<i32>()
+    }
+}
+
+fn main() {
+    use crate::Solution;
+    let nums = vec![0, 1];
+
+    println!("{}", Solution::missing_number(nums));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+    #[test]
+    fn test() {
+        assert_eq!(Solution::missing_number(vec![3, 0, 1]), 2);
+        assert_eq!(Solution::missing_number(vec![0, 1]), 2);
+        assert_eq!(Solution::missing_number(vec![9, 6, 4, 2, 3, 5, 7, 0, 1]), 8);
+        assert_eq!(Solution::missing_number(vec![0]), 1);
+    }
+}
