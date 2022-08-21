68b030fd49e3ab42206c9301bbba681b419599ed
hao <im_leihao@163.com>
2022-08-21 12:10:56 +0800

    add 1455. 检查单词是否为句中其他单词的前缀

---
+++ rust_leetcode/src/bin/is_prefix_of_word.rs
@@ -0,0 +1,49 @@
+struct Solution;
+impl Solution {
+    pub fn is_prefix_of_word(sentence: String, search_word: String) -> i32 {
+        let mut ans = i32::MAX;
+        let mut count = 0;
+
+        for s in sentence.split_ascii_whitespace() {
+            count += 1;
+            if s.starts_with(&search_word) {
+                ans = ans.min(count);
+            }
+        }
+        if ans == i32::MAX {
+            -1
+        } else {
+            ans
+        }
+    }
+}
+
+fn main() {
+    use crate::Solution;
+    let sentence = "i am tired".to_string();
+    let search_word = "you".to_string();
+    println!("{}", Solution::is_prefix_of_word(sentence, search_word));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+    #[test]
+    fn test() {
+        assert_eq!(
+            Solution::is_prefix_of_word("i love eating burger".to_string(), "burg".to_string()),
+            4
+        );
+        assert_eq!(
+            Solution::is_prefix_of_word(
+                "this problem is an easy problem".to_string(),
+                "pro".to_string()
+            ),
+            2
+        );
+        assert_eq!(
+            Solution::is_prefix_of_word("i am tired".to_string(), "you".to_string()),
+            -1
+        );
+    }
+}
